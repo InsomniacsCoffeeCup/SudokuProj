465464b5ea6ad4c82d65c1cbf8403cc4ee386005
Connor <74077667+corgi0@users.noreply.github.com>
2024-11-17 17:33:03 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -7,6 +7,7 @@ https://www.geeksforgeeks.org/program-sudoku-generator/
 """
 ##hello this is johnson
 #hello this is AnnaRose 
+# HELLO THIS IS CONNOR WOOOOOOOOOOOOO
 class SudokuGenerator:
     '''
 	create a sudoku board - initialize class variables and set up the 2D board
